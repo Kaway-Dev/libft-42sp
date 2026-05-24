0e732ed080faa514c4a22988aca3c122f979969a
Philippe <philippekaway@gmail.com>
2026-05-24 17:01:50 -0300

    feat: ft_isalnunm

---
+++ ft_isalnum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ft_isalnum(int c)
+{
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'))
+    {
+        return(1);
+    }
+    return(0);
+}
+
+int main (void)
+{
+    printf("%d\n", ft_isalnum('A'));
+    return(0);
+}
