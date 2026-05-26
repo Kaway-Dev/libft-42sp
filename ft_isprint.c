b41a38bbaaf60c456c4044a5fd00740dbba02b33
Philippe <philippekaway@gmail.com>
2026-05-26 00:40:33 -0300

    feat: implement ft_isprint function

---
+++ ft_isprint.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+
+int ft_isprint(int c)
+{
+    if (c >= 32 && c <= 126)
+    {
+        return (1);
+    }
+    return(0);
+}
+
+// int main(void)
+// {
+//     printf("%d\n", ft_isprint(28));
+//     return(0);
+// }
