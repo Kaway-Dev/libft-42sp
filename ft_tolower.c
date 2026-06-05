8d75c632e8e2de72450ba3a36a916164488b5305
Philippe <philippekaway@gmail.com>
2026-06-05 14:40:07 -0300

    ft_tolower.c

---
+++ ft_tolower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int ft_tolower(int c)
+{
+    if(c >= 'A' && c <= 'Z')
+    {
+        return (c + 32);
+    }
+    return c;
+}
+
+int main(void)
+{
+    printf("%d\n", ft_tolower(65));
+    return(0);
+}
