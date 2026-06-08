43e3df4eef3c8a182c1fe9a4634baac231461fa0
Philippe <philippekaway@gmail.com>
2026-06-07 21:14:13 -0300

    feat: implement ft_bzero function

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,2 +1,25 @@
 #include "libft.h"
 
+void	ft_bzero(void *s, size_t n)
+{
+    unsigned char *ptr;
+    size_t i;
+
+    i = 0;
+    ptr = (unsigned char *)s;
+    while (i < n)
+    {
+        ptr[i] = '\0';
+        i++;
+    }
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     char str[9] = "Gabriela";
+//     ft_bzero(str + 3, 2);
+//     printf("%s\n", str);
+//     return(0);
+// }
