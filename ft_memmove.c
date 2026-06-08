71dff0b3a97da196a101ee23bca81323b6fd3353
Philippe <philippekaway@gmail.com>
2026-06-08 01:14:01 -0300

    feat: implement ft_memmove function

---
+++ ft_memmove.c
@@ -0,0 +1,38 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+    unsigned char *dest_ptr;
+    const unsigned char *src_ptr;
+    size_t i;
+
+    i = 0;
+    dest_ptr = (unsigned char *)dest;
+    src_ptr = (const unsigned char *)src;
+    if (dest_ptr > src_ptr)
+    {
+        while (n > 0)
+        {
+            n--;
+            dest_ptr[n] = src_ptr[n];
+        }
+    }
+    else
+    {
+        while (i < n)
+        {
+            dest_ptr[i] = src_ptr[i];
+            i++;
+        }
+    }
+     return (dest);
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// char str[] = "abcdef";
+// ft_memmove(str + 2, str, 4);
+// printf("%s\n", str);
+// }
