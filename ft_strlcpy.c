ba1ec891ee83fd904c06f2207c75c586ac04b5d4
Philippe <philippekaway@gmail.com>
2026-06-14 13:10:41 -0300

    feat: add ft_strlcpy function implementation

---
+++ ft_strlcpy.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+    
+    size_t i;
+
+    i = 0;
+    if(size > 0)
+    {
+        while(src[i] != '\0' && i < size - 1)
+        {
+            dst[i] = src[i];
+            i++;
+        }
+        dst[i] = '\0';
+    }
+    return ft_strlen(src);
+}
+
+#include <stdio.h>
+int main(void)
+{
+    char src[] = "Gabriela";
+    char dst[5];
+
+    printf("%lu\n", (unsigned long)ft_strlcpy(dst, src, 5));
+    printf("%s\n", dst);
+}
