1c398fc72ad79a4d6f11f5ded8c23b7642e1cd71
Philippe <philippekaway@gmail.com>
2026-06-14 19:27:04 -0300

    feat: add ft_strjoin function implementation

---
+++ ft_strjoin.c
@@ -0,0 +1,33 @@
+#include "libft.h"
+
+char *ft_strjoin(char const *s1, char const *s2)
+{
+    size_t  first_len;
+    size_t  second_len;
+    char    *join;
+
+    first_len = ft_strlen(s1);
+    second_len = ft_strlen(s2);
+    join = malloc(first_len + second_len + 1);
+    if (join == NULL)
+        return (NULL);
+
+    ft_strlcpy(join, s1, first_len + 1);
+    ft_strlcpy(join + first_len, s2, second_len + 1);
+
+    return (join);
+}
+
+// #include <stdlib.h>
+// #include <Stdio.h>
+// int main(void)
+// {
+//     char *join;
+
+//     join = ft_strjoin("Ola ", "pessoal");
+//     if(join == NULL)
+//         return(1);
+//     printf("%s\n", join);
+//     free(join);
+//     return(0);
+// }
