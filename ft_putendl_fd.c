5d624e9c374706a40c2dab503ab162daaa17dbeb
Philippe <philippekaway@gmail.com>
2026-06-16 20:11:08 -0300

    feat: implement ft_putendl_fd function

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -10,3 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_putendl_fd(char *s, int fd)
+{
+    if (!s)
+        return ;
+    ft_putstr_fd(s, fd);
+    ft_putchar_fd('\n', fd);
+}
