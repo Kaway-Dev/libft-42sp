a3af183c2c52d6d4f294600ef8eb82273f109247
Philippe <philippekaway@gmail.com>
2026-06-16 20:38:10 -0300

    fix: correct implementation of ft_putnbr_fd function

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -14,6 +14,24 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+    long nb;
 
+    nb = n;
+    if (nb < 0)
+    {
+        ft_putchar_fd('-', fd);
+        nb = -nb;
+    }
+    if (nb >= 10) 
+        ft_putnbr_fd(nb / 10, fd);
 
+    ft_putchar_fd(nb % 10 + '0', fd);  
 }
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	ft_putnbr_fd(-2147483648, 1);
+// 	ft_putchar_fd('\n', 1);
+// 	return (0);
+// }
