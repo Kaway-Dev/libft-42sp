df667841994afff3d1a047226e0dd345fbeda9bb
Philippe <philippekaway@gmail.com>
2026-06-18 13:37:28 -0300

    feat: implement word counting and memory management functions in ft_split

--- ft_split.c
+++ ft_split.c
@@ -10,3 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+static  int count_words(char const *s, char c)
+{
+    int i;
+    int count;
+
+    i = 0;
+    count = 0;
+    while (s[i] != '\0')
+    {
+        while (s[i] != '\0' && s[i] == c)
+        i++;
+        if(s[i] != '\0')
+            count++;
+        while(s[i] != '\0' && s[i] != c)
+        i++;
+    }
+    return (count);
+}
+
+static  int word_len(char const *s, char c)
+{
+    int i;
+
+    i = 0;
+    while (s[i] != '\0' && s[i] != c)
+        i++;
+    return (i);
+}
+
+static  char **split_free(char **result, int i)
+{
+    while(i > 0)
+    {
+        i--;
+        free(result[i]);
+    }
+    free(result);
+    return (NULL);
+}
