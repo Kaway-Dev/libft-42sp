f29a0dce994f16685e3189c02e7a55c755375ff3
Philippe <philippekaway@gmail.com>
2026-06-18 12:07:50 -0300

    refactor: rename variable for clarity in count_len function

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,21 +14,20 @@
 
 static  int count_len(long nb)
 {
-    unsigned int    numbers;
+    unsigned int    count;
 
     if (nb == 0)
         return (1);
     if (nb < 0)
-        numbers = 1;
+        count = 1;
     else
-        numbers = 0;
-
+        count = 0;
     while (nb != 0)
     {
-        numbers++;
-        nb = nb/10;
+        count++;
+        nb = nb / 10;
     }
-    return (numbers);
+    return (count);
 }
 
 char	*ft_itoa(int n)
