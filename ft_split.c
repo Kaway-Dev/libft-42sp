a6c6f35e454d613fde9f87886094e301b99598da
Kaway <philippekaway@gmail.com>
2026-06-18 23:41:03 -0300

    fix: update author information in ft_split.c and improve code formatting

--- ft_split.c
+++ ft_split.c
@@ -3,53 +3,53 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phkaway- <phkaway-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: philippe <philippe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 21:32:15 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/06/17 21:32:16 by phkaway-         ###   ########.fr       */
+/*   Updated: 2026/06/18 23:38:57 by philippe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static  int count_words(char const *s, char c)
+static int	count_words(char const *s, char c)
 {
-    int i;
-    int count;
+	int	i;
+	int	count;
 
-    i = 0;
-    count = 0;
-    while (s[i] != '\0')
-    {
-        while (s[i] != '\0' && s[i] == c)
-        i++;
-        if(s[i] != '\0')
-            count++;
-        while(s[i] != '\0' && s[i] != c)
-        i++;
-    }
-    return (count);
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		if (s[i] != '\0')
+			count++;
+		while (s[i] != '\0' && s[i] != c)
+			i++;
+	}
+	return (count);
 }
 
-static  int word_len(char const *s, char c)
+static int	word_len(char const *s, char c)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (s[i] != '\0' && s[i] != c)
-        i++;
-    return (i);
+	i = 0;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
+	return (i);
 }
 
-static  char **split_free(char **result, int i)
+static char	**split_free(char **result, int i)
 {
-    while(i > 0)
-    {
-        i--;
-        free(result[i]);
-    }
-    free(result);
-    return (NULL);
+	while (i > 0)
+	{
+		i--;
+		free(result[i]);
+	}
+	free(result);
+	return (NULL);
 }
 
 static char	**fill_split(char **result, char const *s, char c)
@@ -80,8 +80,8 @@ static char	**fill_split(char **result, char const *s, char c)
 
 char	**ft_split(char const *s, char c)
 {
-    char	**result;
-	int		words;
+	char		**result;
+	int			words;
 
 	if (s == NULL)
 		return (NULL);
