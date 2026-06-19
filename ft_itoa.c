3639b65d19a3776d3c8ffaa68803c91f4e8b015e
Kaway <philippekaway@gmail.com>
2026-06-18 23:51:45 -0300

    fix: improve code formatting in ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,56 +12,56 @@
 
 #include "libft.h"
 
-static  int count_len(long nb)
+static int	count_len(long nb)
 {
-    unsigned int    count;
+	unsigned int	count;
 
-    if (nb == 0)
-        return (1);
-    if (nb < 0)
-        count = 1;
-    else
-        count = 0;
-    while (nb != 0)
-    {
-        count++;
-        nb = nb / 10;
-    }
-    return (count);
+	if (nb == 0)
+		return (1);
+	if (nb < 0)
+		count = 1;
+	else
+		count = 0;
+	while (nb != 0)
+	{
+		count++;
+		nb = nb / 10;
+	}
+	return (count);
 }
 
-static  void fill_str(char *str, long nb, unsigned int len)
+static void	fill_str(char *str, long nb, unsigned int len)
 {
-    str[len] = '\0';
-    if (nb == 0)
-    {
-        str[0] = '0';
-        return ;
-    }
-    if (nb < 0)
-    {
-        str[0] = '-';
-        nb = -nb;
-    }
-    while (nb > 0)
-    {
-        str[len - 1] = nb % 10 + '0';
-        nb = nb / 10;
-        len--;
-    }
+	str[len] = '\0';
+	if (nb == 0)
+	{
+		str[0] = '0';
+		return ;
+	}
+	if (nb < 0)
+	{
+		str[0] = '-';
+		nb = -nb;
+	}
+	while (nb > 0)
+	{
+		str[len - 1] = nb % 10 + '0';
+		nb = nb / 10;
+		len--;
+	}
 }
 
 char	*ft_itoa(int n)
 {
-    unsigned int    len;
-    long            nb;
-    char            *str;
+	unsigned int		len;
+	long				nb;
+	char				*str;
 
-    nb = n;
-    len = count_len(nb);
-    str = malloc(len + 1);
-    if (str == NULL)
-        return (NULL);
-    fill_str(str, nb, len);
-    return (str);
+	nb = n;
+	len = count_len(nb);
+	str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+	fill_str(str, nb, len);
+	return (str);
 }
