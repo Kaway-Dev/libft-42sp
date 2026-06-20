617ccc5678ab8c9fcc19674bd434b1cdd45a4ab2
Philippe Kaway Pontes Sales <phkaway-@c2r10p4.42sp.org.br>
2026-06-20 11:52:27 -0300

    update lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,33 +6,44 @@
 /*   By: phkaway- <phkaway-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/17 21:04:59 by phkaway-          #+#    #+#             */
-/*   Updated: 2026/06/17 21:26:25 by phkaway-         ###   ########.fr       */
+/*   Updated: 2026/06/20 11:51:45 by phkaway-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static	t_list	*ft_clear(t_list **lst, void (*del)(void *), void *content)
+{
+	if (content)
+		del(content);
+	ft_lstclear(lst, del);
+	return (NULL);
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*new_node;
-	void	*new_content;
+	t_list	*new_head;
+	t_list	*new;
+	t_list	*tail;
+	void	*content;
 
-	if (f == NULL || del == NULL)
+	if (!lst || !f || !del)
 		return (NULL);
-	new_list = NULL;
-	while (lst != NULL)
+	new_head = NULL;
+	while (lst)
 	{
-		new_content = f(lst->content);
-		new_node = ft_lstnew(new_content);
-		if (new_node == NULL)
-		{
-			del(new_content);
-			ft_lstclear(&new_list, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_list, new_node);
+		content = f(lst->content);
+		if (!content)
+			return (ft_clear(&new_head, del, content));
+		new = ft_lstnew(content);
+		if (!new)
+			return (ft_clear(&new_head, del, content));
+		if (!new_head)
+			new_head = new;
+		else
+			tail->next = new;
+		tail = new;
 		lst = lst->next;
 	}
-	return (new_list);
+	return (new_head);
 }
